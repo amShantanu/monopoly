96ccda9615f408cffdc74ee814888e918dacecff
abhinavanand-fed <abhinavanand080@gmail.com>
2023-02-17 00:49:40 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./Board.css";
 
-function App() {
+const BoardSquare = ({ position, playerIndex, currentPlayer }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="board-square"
+      style={{
+        backgroundColor:
+          playerIndex === currentPlayer
+            ? "lightblue"
+            : position % 2 === 0
+            ? "#F2F2F2"
+            : "#E6E6E6"
+      }}
+    >
+      {playerIndex !== -1 ? `Player ${playerIndex + 1}` : position + 1}
     </div>
   );
-}
+};
 
-export default App;
+const Dice = ({ onRoll, disabled }) => {
+  const handleClick = () => {
+    if (!disabled) {
+      const roll = Math.floor(Math.random() * 6) + 1;
+      onRoll(roll);
+    }
+  };
+
+  return (
+    <button onClick={handleClick} disabled={disabled} className="dice-button">
+      Roll dice
+    </button>
+  );
+};
+
+const Board = () => {
+  const [playerPositions, setPlayerPositions] = useState([0, 0]);
+  const [turn, setTurn] = useState(0);
+  const [winner, setWinner] = useState(-1);
+  const [turnDone, setTurnDone] = useState(false);
+  const currentPlayer = winner === -1 ? turn : -1;
+
+  const movePlayer = (playerIndex, diceRoll) => {
+    if (winner !== -1 || (turnDone && playerIndex === turn)) {
+      return;
+    }
+    const newPositions = [...playerPositions];
+    newPositions[playerIndex] += diceRoll;
+    if (newPositions[playerIndex] >= 40) {
+      setWinner(playerIndex);
+      return;
+    }
+    setTurnDone(true);
+    setPlayerPositions(newPositions);
+  };
+
+  const nextTurn = () => {
+    setTurn(turn === 0 ? 1 : 0);
+    setTurnDone(false);
+  };
+
+  const board = [];
+  for (let i = 0; i < 40; i++) {
+    board.push(
+      <BoardSquare
+        key={i}
+        position={i}
+        playerIndex={playerPositions.indexOf(i)}
+        currentPlayer={currentPlayer}
+      />
+    );
+  }
+
+  return (
+    <div className="board-container">
+      {winner !== -1 && (
+        <div className="winner">
+          Player {winner + 1} wins!
+        </div>
+      )}
+      <div className="board">
+        {board}
+      </div>
+      {winner === -1 && (
+        <>
+          <Dice
+            onRoll={diceRoll => movePlayer(turn, diceRoll)}
+            disabled={turnDone}
+          />
+          {turnDone && (
+            <button onClick={nextTurn} className="next-turn-button">
+              Next turn
+            </button>
+          )}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Board
